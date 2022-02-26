81ab0b2e0f7986441ad07588b4763b86dee37519
moggishere <aryyoon@gmail.com>
2022-02-26 00:01:09 -0300

    added source map

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import './style.css'
 
 function component() {
 
+    const testing = document.getElementById('buttons-projects-add');
+
+    testing.addEventListener('click', e => {
+        e.preventDefault;
+        console.log('you clicked +')
+    })
+
 }
 
 document.body.appendChild(component());
